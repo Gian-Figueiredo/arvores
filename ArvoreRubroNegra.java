e888e3b4ca075e0a4cada12758de12395360d1ec
Gian-Fedora <gian78367@gmail.com>
2025-12-13 18:45:49 -0300

    Começando a criar as ideias da árvore

--- ArvoreRubroNegra.java
+++ ArvoreRubroNegra.java
@@ -1,5 +1,30 @@
 public class ArvoreRubroNegra {
+
+    private enum Color {
+        VERMELHA, PRETA;
+    }
+
+    private class Node {
+        private int data;
+        private Color color;
+        private Node left_child;
+        private Node right_child;
+
+        Node(int valor, Color cor, Node filho_esquerda, Node filho_direita) {
+            this.data = valor;
+            this.color = cor;
+            this.left_child = filho_esquerda;
+            this.right_child = filho_direita;
+        }
+
+        Node(int valor) {
+            this.data = valor;
+            this.color = Color.PRETA;
+            this.left_child = null;
+            this.right_child = null;
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("Reali eh gay");
+        
     }
 }
