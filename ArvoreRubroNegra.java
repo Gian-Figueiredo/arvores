954ba2a6cd85f5707888b13b62d8f1b93046e60b
Gian-Fedora <gian78367@gmail.com>
2025-12-14 16:09:00 -0300

    Recomeço de tudo: Deu tudo errado

--- ArvoreRubroNegra.java
+++ ArvoreRubroNegra.java
@@ -1,16 +1,46 @@
 public class ArvoreRubroNegra {
 
     private enum Color {
-        VERMELHA, PRETA;
+        RED, BLACK;
     }
 
     private class Node {
-        
+        public Integer data;
+        public Color color;
+        public Node leftChild;
+        public Node rigthChild;
+
+        Node (Integer valor, Color cor, Node filhoMenor, Node filhoMaior) {
+            this.data = valor;
+            this.color = cor;
+            this.leftChild = filhoMenor;
+            this.rigthChild = filhoMaior;
+        }
+
+        public void swapColor() {
+            if (this.color == Color.RED) {
+                this.color = Color.BLACK;
+            } else {
+                this.color = Color.RED;
+            }
+        }
+
     }
 
     private Node root;
+    private final Node NIL = new Node(null, Color.BLACK, null, null);
+
+    ArvoreRubroNegra() {
+        this.root = NIL;
+    }
+
+    
+
+    public void insert(int x) {
+        
+    }
 
     public static void main(String[] args) {
-        
+
     }
 }
