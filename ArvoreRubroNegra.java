fb9ae25be4e31d47a13f6a47f8e5b4af4309c90e
Gian-Fedora <gian78367@gmail.com>
2025-12-14 13:58:19 -0300

    De volta a estaca -1

--- ArvoreRubroNegra.java
+++ ArvoreRubroNegra.java
@@ -5,61 +5,12 @@ public class ArvoreRubroNegra {
     }
 
     private class Node {
-        private Integer data;
-        private Color color;
-        private Node leftChild;
-        private Node rigthChild;
-
-        Node(Integer valor, Color cor, Node filhoEsquerda, Node filhoDireita) {
-            this.data = valor;
-            this.color = cor;
-            this.leftChild = filhoEsquerda;
-            this.rigthChild = filhoDireita;
-        }
-
-        Node(Integer valor) {
-            this.data = valor;
-            this.color = Color.PRETA;
-            this.leftChild = null;
-            this.rigthChild = null;
-        }
-
-        private String ordem() {
-            if (this.data == null) {
-                return "(null - preto)";
-            } else {
-                return this.leftChild.ordem() + " - " + this.data + " - " + this.rigthChild.ordem();
-            }
-        }
-
+        
     }
 
     private Node root;
 
-    ArvoreRubroNegra() {
-        this.root = new Node(null);
-    }
-
-    public void insert(int data) {
-        if (this.root.data == null) {
-            this.root = new Node(data);
-            return;
-        }
-        Node no = this.root;
-        while (no.data != null) {
-            if (data > no.data) {
-                no = no.rigthChild;
-            } else {
-                no = no.leftChild;
-            }
-        }
-        no = new Node(data);
-    }
-
-    public String toString() {
-        return this.root.ordem();
-    }
-
     public static void main(String[] args) {
-
+        
+    }
 }
