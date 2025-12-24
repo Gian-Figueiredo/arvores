8b81df700d0ef932aa44e6b582470fa1698a446a
Gian-Fedora <gian78367@gmail.com>
2025-12-23 21:20:29 -0300

    Consertando todos os bugs acumulados

--- ArvoreRB.java
+++ ArvoreRB.java
@@ -34,11 +34,11 @@ public class ArvoreRB {
         this.root = null;
     }
 
-    public boolean search(int x) {
+    private Node findNode(int x) {
         Node node = this.root;
         while (node.key != null) {
             if (node.key == x) {
-                return true;
+                return node;
             }
             if (x < node.key) {
                 node = node.left;
@@ -46,7 +46,11 @@ public class ArvoreRB {
                 node = node.right;
             }
         }
-        return false;
+        return null;
+    }
+
+    public boolean search(int x) {
+        return findNode(x) != null;
     }
 
     private Node brothers(Node node) {
@@ -64,6 +68,9 @@ public class ArvoreRB {
 
     private void rightRotation(Node node, Node parent, Node grandparent) {
         grandparent.right = new Node(grandparent.key, !grandparent.color, parent.right, grandparent.right);
+        parent.right.parent = grandparent.right;
+        parent.right = null;
+        grandparent.right.right.parent = grandparent.right;
         grandparent.right.parent = grandparent;
 
         grandparent.key = parent.key;
@@ -76,6 +83,9 @@ public class ArvoreRB {
 
     private void leftRotation(Node node, Node parent, Node grandparent) {
         grandparent.left = new Node(grandparent.key, !grandparent.color, grandparent.left, parent.left);
+        parent.left.parent = grandparent.left;
+        parent.left = null;
+        grandparent.left.left.parent = grandparent.left;
         grandparent.left.parent = grandparent;
 
         grandparent.key = parent.key;
@@ -88,6 +98,9 @@ public class ArvoreRB {
 
     private void doubleRightRotation(Node node, Node parent, Node grandparent) {
         grandparent.right = new Node(grandparent.key, !grandparent.color, node.right, grandparent.right);
+        grandparent.right.right.parent = grandparent.right;
+        node.right.parent = grandparent.left;
+        node.right = null;
         grandparent.right.parent = grandparent;
 
         grandparent.key = node.key;
@@ -95,10 +108,14 @@ public class ArvoreRB {
         parent.right.parent = null;
         parent.right = node.left;
         parent.right.parent = parent;
+        node.left = null;
     }
 
     private void doubleLeftRotation(Node node, Node parent, Node grandparent) {
         grandparent.left = new Node(grandparent.key, !grandparent.color, grandparent.left, node.left);
+        grandparent.left.left.parent = grandparent.left;
+        node.left.parent = grandparent.left;
+        node.left = null;
         grandparent.left.parent = grandparent;
 
         grandparent.key = node.key;
@@ -106,6 +123,7 @@ public class ArvoreRB {
         parent.left.parent = null;
         parent.left = node.right;
         parent.left.parent = parent;
+        node.right = null;
     }
 
     private void rotacao(Node node, Node parent, Node grandparent) {
@@ -126,15 +144,14 @@ public class ArvoreRB {
 
         if (!isLeftChild(node, parent) && !isLeftChild(parent, grandparent)) {
             leftRotation(node, parent, grandparent);
-            return;
         }
     }
 
-    private void balancear(Node node) {
-        if (node.parent.color == NEGRO) {
+    private void balancearInsecao(Node node, Node parent) {
+        if (parent.color == NEGRO) {
             return;
         }
-        if (node.parent.color == RUBRO && grandparent(node).color == NEGRO && brothers(node.parent).color == RUBRO) {
+        if (parent.color == RUBRO && parent.parent.color == NEGRO && brothers(parent).color == RUBRO) {
             Node a, p, t;
             a = grandparent(node);
             p = node.parent;
@@ -144,16 +161,14 @@ public class ArvoreRB {
             p.switchColor();
             t.switchColor();
 
-            if (a.key == this.root.key || a.parent.key == this.root.key) {
-                return;
-            } else {
-                balancear(a);
-                return;
+            if (a.key != this.root.key && a.parent.color == RUBRO) {
+                balancearInsecao(a, a.parent);
             }
+            return;
         }
-        if (node.parent.color == RUBRO && grandparent(node).color == NEGRO && brothers(node.parent).color == NEGRO) {
+        if (parent.color == RUBRO && parent.parent.color == NEGRO && brothers(parent).color == NEGRO) {
             //rotações
-            rotacao(node, node.parent, grandparent(node));
+            rotacao(node, parent, grandparent(node));
         }
     }
 
@@ -183,14 +198,179 @@ public class ArvoreRB {
         if (x < node.key) {
             node.left = new Node(x, RUBRO, nil, nil);
             node.left.parent = node;
-            balancear(node.left);
+            balancearInsecao(node.left, node);
         } else {
             node.right = new Node(x, RUBRO, nil, nil);
             node.right.parent = node;
-            balancear(node.right);
+            balancearInsecao(node.right, node);
         }
         if (this.root.color == RUBRO) {
             this.root.switchColor();
+        }
+    }
+
+    private void removeNodeSimple(Node node) {
+        // Como o nó a ser removido fisicamente será o sucessor, ele possui no máximo um filho direto.
+        Node parent = node.parent;
+        if (isLeftChild(node, parent)) {
+            parent.left = node.right;
+            node.right.parent = parent;
+        } else {
+            parent.right = node.left;
+            parent.left.parent = parent;
+        }
+    }
+
+    private Node findSucessor(Node node) {
+        if (node.right.key == null) {
+            return null;
+        }
+        Node next = node.right;
+        while (next.left.key != null) {
+            next = next.left;
+        }
+        return next;
+    }
+
+    private Node findAntecessor(Node node) {
+        if (node.left.key == null) {
+            return null;
+        }
+        Node next = node.left;
+        while (next.right.key != null) {
+            next = next.right;
+        }
+        return next;
+    }
+
+    public void remove(int x) {
+        if (this.root == null) {
+            System.out.println("Árvore vazia");
+            return;
+        }
+
+        Node node = findNode(x);
+
+        if (node == null) {
+            System.out.println("Valor não encontrado na árvore");
+            return;
+        }
+
+        removeNode(node);
+    }
+
+    private void balancearDuploNegro(Node node, Node parent, Node brother) {
+        if (isLeftChild(node, parent)) {
+            if (brother.color == RUBRO) {
+                leftRotation(brother.right, brother, parent);
+                balancearDuploNegro(node, node.parent, brothers(node));
+                return;
+            } else {
+                //Caso o irmão tenha a cor negra
+                if (brother.right.color == NEGRO && brother.left.color == NEGRO) {
+                    brother.switchColor(); //Muda para RUBRO
+                    if (parent.color == RUBRO) {
+                        parent.switchColor(); //MUDA para NEGRO
+                    } else {
+                        if (parent.key == this.root.key) {
+                            //Se o pai for a raíz, então a árvore está balanceada novamente e podemos parar
+                            return;
+                        }
+                        balancearDuploNegro(parent, parent.parent, brothers(parent));
+                    }
+                    return;
+                }
+
+                if (brother.right.color == RUBRO) {
+                    leftRotation(parent, brother, brother.right);
+                    parent.left.color = NEGRO;
+                    parent.right.color = NEGRO;
+                    if (this.root.color == RUBRO) {
+                        this.root.switchColor();
+                    }
+                    return;
+                }
+
+                if (brother.right.color == NEGRO) {
+                    rightRotation(brother.left.left, brother.left, brother);
+                    balancearDuploNegro(node, node.parent, brothers(node));
+                    return;
+                }
+            }
+        } else {
+            if (brother.color == RUBRO) {
+                rightRotation(brother.left, brother, parent);
+                balancearDuploNegro(node, node.parent, brothers(node));
+                return;
+            } else {
+                //Caso o irmão tenha a cor negra
+                if (brother.left.color == NEGRO && brother.right.color == NEGRO) {
+                    brother.switchColor(); //Muda para RUBRO
+                    if (parent.color == RUBRO) {
+                        parent.switchColor(); //MUDA para NEGRO
+                    } else {
+                        if (parent.key == this.root.key) {
+                            //Se o pai for a raíz, então a árvore está balanceada novamente e podemos parar
+                            return;
+                        }
+                        balancearDuploNegro(parent, parent.parent, brothers(parent));
+                    }
+                    return;
+                }
+
+                if (brother.left.color == RUBRO) {
+                    rightRotation(parent, brother, brother.right);
+                    parent.right.color = NEGRO;
+                    parent.left.color = NEGRO;
+                    if (this.root.color == RUBRO) {
+                        this.root.switchColor();
+                    }
+                    return;
+                }
+
+                if (brother.left.color == NEGRO) {
+                    leftRotation(brother.right.right, brother.right, brother);
+                    balancearDuploNegro(node, node.parent, brothers(node));
+                    return;
+                }
+            }
+        }
+    }
+
+    private void removeNode(Node node) {
+        if (node.left.key == null && node.right.key == null) {
+            if (node.color == RUBRO) {
+                removeNodeSimple(node);
+            } else if (node.color == NEGRO) {
+                node.key = null;
+                balancearDuploNegro(node, node.parent, brothers(node));
+            }
+            return;
+        }
+
+        Node sucessor = findSucessor(node);
+        Node antecessor = findAntecessor(node);
+        Node descendente = (sucessor == null) ? antecessor : sucessor;
+
+        if (descendente.color == RUBRO) {
+            node.key = descendente.key;
+            removeNodeSimple(descendente);
+            return;
+        }
+
+        if (descendente.color == NEGRO) {
+            if (node.color == RUBRO) {
+                Integer temp = node.key;
+                node.key = descendente.key;
+                descendente.key = temp;
+                removeNode(descendente);
+                return;
+            } else if (node.color == NEGRO) {
+                node.key = descendente.key;
+                descendente.key = null;
+                balancearDuploNegro(descendente, descendente.parent, brothers(descendente));
+                return;
+            }
         }
     }
 
@@ -211,9 +391,9 @@ public class ArvoreRB {
 
     public static void main(String[] args) {
         ArvoreRB a = new ArvoreRB();
-        System.out.println(a);
-        int[] b = {50,40,60,70,55,80,65,90};
+        int[] b = {90,100,110,120,50,60,70,80,10,20,30,40,130,140,150,160};
         for (int i = 0; i < b.length; i++) {
+            System.out.println(a);
             a.insert(b[i]);
         }
         System.out.println(a);
--- README.md
+++ README.md
@@ -3,9 +3,9 @@ Trabalho feito por Gian Victor Gonçalves Figueiredo e Felipe Lisboa Brasil.
 
 Os arquivos estarão em Java, e os arquivos compilados estarão na pasta bin.
 
-O arquivo `ArvoreRubroNegra.java` refere-se às Árvores Rubro Negras e o arquivo `Avlarquivo.java` refere-se às Árvores AVL.
+O arquivo `ArvoreRB.java` refere-se às Árvores Rubro Negras e o arquivo `Avlarquivo.java` refere-se às Árvores AVL.
 
 # Compilar e Executar
-Para compilar o código da árvore rubro negra, use `javac -d bin ArvoreRubroNegra.java` e para e executar use `java -cp bin ArvoreRubroNegra`
+Para compilar o código da árvore rubro negra, use `javac -d bin ArvoreRB.java` e para e executar use `java -cp bin ArvoreRB`
 
 Para compilar o código da árvore rubro negra, use `javac -d bin Avlarquivo.java` e para e executar use `java -cp bin Avlarquivo`
